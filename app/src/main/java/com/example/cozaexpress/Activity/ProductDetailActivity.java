7f468575c137036c733a83bb4d5b0d53b5d41ea6
Hoang Toan Nguyen <hoangtoannguyen@Toans-MacBook-Pro-2.local>
2023-04-06 15:42:53 +0700

    fragment vertical

--- app/src/main/java/com/example/cozaexpress/Activity/ProductDetailActivity.java
+++ app/src/main/java/com/example/cozaexpress/Activity/ProductDetailActivity.java
@@ -1,16 +1,24 @@
 package com.example.cozaexpress.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.cozaexpress.Adapter.ProDetailAdapter;
 import com.example.cozaexpress.Model.Product;
 import com.example.cozaexpress.R;
 import com.example.cozaexpress.api.APIService;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.List;
 
@@ -25,17 +33,23 @@ public class ProductDetailActivity extends AppCompatActivity {
 
     List<Product> productList;
 
+    TabLayout mTablayout;
+
+    ViewPager2 mViewPager;
+
+    AppCompatButton btnThemVaoGio, btnMuaNgay;
+
+    ProDetailAdapter proDetailAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
+        AnhXa();
 
-        imgProduct = findViewById(R.id.imgPro_detail);
-        tvPrice = findViewById(R.id.tvProprice);
-        tvDesciption = findViewById(R.id.tvProDesciption);
-
-        Intent intent = getIntent();
-        Bundle bundle = intent.getExtras();
+//        Intent intent = getIntent();
+//        Bundle bundle = intent.getExtras();
         //Product product = (Product) bundle.getSerializable("product");
 
 //        if (product != null) {
@@ -46,28 +60,45 @@ public class ProductDetailActivity extends AppCompatActivity {
 //            tvDesciption.setText(product.getDesciption());
 //        }
 
-        String id = bundle.getString("idProduct");
+        //String id = bundle.getString("idProduct");
 
-        getProductById(id);
+        ///getProductById(id);
 
 
     }
 
-    private void getProductById(String id) {
-            APIService.apiService.getProductById(id).enqueue(new Callback<Product>() {
-                @Override
-                public void onResponse(Call<Product> call, Response<Product> response) {
-                    Product product = response.body();
-                    Glide.with(getApplicationContext())
-                    .load(product.getListimage())
-                    .into(imgProduct);
-                    tvPrice.setText(product.getPrice().toString());
-                    tvDesciption.setText(product.getDesciption());
-                }
-                @Override
-                public void onFailure(Call<Product> call, Throwable t) {
+    private void AnhXa() {
+        mTablayout = findViewById(R.id.tab_pro_detail);
+        mViewPager = findViewById(R.id.view_pager_pro_detail);
 
+        btnThemVaoGio = findViewById(R.id.btnAddToCart);
+        btnMuaNgay = findViewById(R.id.btnMuaNgay);
+
+        proDetailAdapter = new ProDetailAdapter(this);
+        mViewPager.setAdapter(proDetailAdapter);
+        mViewPager.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
+        new TabLayoutMediator(mTablayout, mViewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position){
+                    case 0:
+                        tab.setText("Tổng Quan");
+                        break;
+                    case 1:
+                        tab.setText("Mô Tả");
+                        break;
+                    case 2:
+                        tab.setText("Thông Số");
+                        break;
+                    case 3:
+                        tab.setText("HDSD");
+                        break;
+                    case 4:
+                        tab.setText("Đánh Giá");
+                        break;
                 }
-            });
+            }
+        }).attach();
     }
+
 }
